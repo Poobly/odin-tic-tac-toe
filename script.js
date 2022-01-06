950699a84a9d4e33fcb0c86b51ea159a59e92e5e
Tommy <tommychabiras@gmail.com>
2022-01-06 02:39:04 -0500

    added main structure to tic tac toe, need to fix turnhandler

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", ""];
-    return {gameboard}
+    return {gameboard};
 })();
 
 const displayController = (() => {
@@ -8,23 +8,42 @@ const displayController = (() => {
     const display = (gameboard) => {
         console.log(gameboard);
     };
-    return {display}
+    return {display};
 })();
 
-const Players = (name) => {
+const Players = (name, sign) => {
     const getName  = () => name;
     const setSquare = (button) => {
 
     };
-    return {setSquare};
+    const getSign = () => sign;
+    return {getName, getSign};
 }
 
+const turnHandler = (() => {
+    let turn;
+    const getTurn = () => turn;
+    const nextTurn = () => (turn !== p1) ? turn = p1 : turn = p2;
+    return {getTurn, nextTurn};
+})();
+
 const playGame = (() => {
-    const divs = document.querySelectorAll(".grid-piece");
+    const divs = document.querySelectorAll(".grid-piece"); 
+    const p1 = Players("p1", "X");
+    const p2 = Players("p2", "O");
+    console.log(p1.getSign());
     (function createListeners() {
         divs.forEach((div) => {
             div.addEventListener("click", (e) => {
-                gameBoard.gameboard[e.target.dataset.key] = ;
+                switch (turnHandler.getTurn(p1, p2)) {
+                    case p1:
+                        gameBoard.gameboard[e.target.dataset.key] = p1.getSign();
+                        break;
+                    case p2:
+                        gameBoard.gameboard[e.target.dataset.key] = p2.getSign();
+                        break;
+                }
+                turnHandler.nextTurn();
             });
         });
     })();
